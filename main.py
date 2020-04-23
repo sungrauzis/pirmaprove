5d29a72f721b3394d3dbd50e79d301589d2ed7d4
sungrauzis <60142359+sungrauzis@users.noreply.github.com>
2020-04-23 11:36:49 +0300

    parmums

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ def divi():
     return render_template('divi.html')
 
 
-
 @app.route('/galerija')
 def galerija():
     return render_template('galerija.html')
 
+
 @app.route('/veidotaji')
 def veidotaji():
     return render_template('veidotaji.html')
+
+
+@app.route('/parmums')
+def parmums():
+    return render_template('parmums.html')
+
 
 if __name__ == "__main__":
   app.run(debug=True)
